96e8ad09b3764324235cfb14754058847e732b3b
League Student <league@iMac-18.attlocal.net>
2020-01-06 19:57:58 -0800

    start of the level

--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,25 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Enter a message");
+		for (int i = 0; i > input.length(); i++) {
+			
+		}
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/InputText.txt");
+			fw.write(input);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
